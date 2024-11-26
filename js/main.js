28803e79b1b7ee77578184b14de96d7c4c8435c6
Axel Alva <axelalva2021@gmail.com>
2024-11-26 17:12:07 -0300

    agregando js

--- js/main.js
+++ js/main.js
@@ -30,3 +30,34 @@ function escribirEfecto() {
 }
 
 escribirEfecto();
+
+const fotoCard1 =  document.getElementById("fotoCard1")
+
+fotoCard1.addEventListener("mouseenter", () =>{
+    fotoCard1.src = "./img/perro-hambre.jpg"
+})
+
+fotoCard1.addEventListener("mouseleave", () => {
+    fotoCard1.src = "./img/carne-card.jpg"; // Imagen original o de reemplazo
+});
+
+const fotoCard2 =  document.getElementById("fotoCard2")
+
+fotoCard2.addEventListener("mouseenter", () =>{
+    fotoCard2.src = "./img/perro-hambre.jpg"
+})
+
+fotoCard2.addEventListener("mouseleave", () => {
+    fotoCard2.src = "./img/pescado-card.jpg"; // Imagen original o de reemplazo
+});
+
+const fotoCard3 =  document.getElementById("fotoCard3")
+
+fotoCard3.addEventListener("mouseenter", () =>{
+    fotoCard3.src = "./img/perro-hambre.jpg"
+})
+
+fotoCard3.addEventListener("mouseleave", () => {
+    fotoCard3.src = "./img/pollo-card.jpg"; // Imagen original o de reemplazo
+});
+
